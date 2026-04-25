305684d53674364fd071b72f3e76aae9b61df432
Mayank Shrivastava <mayankshrivastava179@gmail.com>
2026-04-26 04:12:35 +0530

    working on the layout file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        lightGray: "#F5F5F5",
+        darkGray: "#333333",
+      },
+    },
+  },
+  plugins: [],
+};
